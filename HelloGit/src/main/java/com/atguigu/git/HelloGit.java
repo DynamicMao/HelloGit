058574a10661a2ee8013a38ada6f0286d1bc5264
xiaomao <867757465@qq.com>
2020-08-23 16:43:19 +0800

    第一次在IDEA中使用Git，添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author 小毛
+ * @create 2020-08-23 16:32
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git！");
+    }
+}
